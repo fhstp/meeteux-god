5f8b46600ecc7f3bd8f560c8356c192d0de03dce
Schlese <niklas.thuer@gmail.com>
2017-10-24 14:25:31 +0200

    Updated pm2 ecosystem files
    Added new ecosystem file for development server

--- ecosystem_dev.config.js
+++ ecosystem_dev.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     apps: [
         {
-            name:"MEETeUX-GoD",
+            name:"MEETeUX-GoD-Test",
             script: "dist/index.js",
             env_production: {
                 NODE_ENV: 'production'
@@ -9,12 +9,12 @@ module.exports = {
         }
     ],
     deploy: {
-        production: {
+        develop: {
             user: 'node',
             host: 'god.meeteux.fhstp.ac.at',
-            ref: 'origin/master',
+            ref: 'origin/develop',
             repo: "https://github.com/fhstp/meeteux-god.git",
-            path: '/srv/production',
+            path: '/srv/develop',
             'post-deploy': 'cp ../.env ./ && npm install && pm2 startOrRestart ecosystem.config.js --env production'
         }
     }
