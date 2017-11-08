791011fc8015421cd5ac0f84dbec5b708b350228
Schlese <niklas.thuer@gmail.com>
2017-11-08 11:35:19 +0100

    Updated pm2
    updated ecosystem.config file to set the cwd property

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,12 +9,12 @@ module.exports = {
         }
     ],
     deploy: {
-        production: {
+        develop: {
             user: 'node',
             host: 'god.meeteux.fhstp.ac.at',
             ref: 'origin/master',
             repo: "https://github.com/fhstp/meeteux-god.git",
-            path: '/srv/production',
+            path: '/srv/develop',
             'post-deploy': 'cp ../.env ./ && npm install && pm2 startOrRestart ecosystem.config.js --env production'
         }
     }
