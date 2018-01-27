b102ee0e017b0d60349f5068de796acf2198201a
Schlese <niklas.thuer@gmail.com>
2018-01-27 16:34:29 +0100

    Implemented https connection
    * Updated server to run with https

--- src/config/server.ts
+++ src/config/server.ts
@@ -1,7 +1,10 @@
 import * as Express from 'express';
-import * as http from 'http';
+import * as fs from 'fs';
+import * as https from 'https';
 import  { WebSocket } from '../websockets';
 require('dotenv').config();
+
+//import * as http from 'http';
 
 export default class Server
 {
@@ -11,18 +14,31 @@ export default class Server
 
     constructor()
     {
+        const cred = this.loadCredentials();
+
         this.app = new Express();
-        this.server = new http.Server(this.app);
+        this.server = https.createServer(cred, this.app);
         this.socket = new WebSocket(this.server);
 
         console.log('Server runs on Port: ' + process.env.SERVER_PORT);
         this.server.listen(process.env.SERVER_PORT);
 
-        //this.app.get('/', function (req, res)
-        //{
-            //console.log(process.env.NODE_PATH);
-           // res.sendFile(process.env.NODE_PATH + '/assets/localIndex.html');
-        //});
+        this.app.get('/', function (req, res)
+        {
+            console.log(process.env.NODE_PATH);
+            res.sendFile(process.env.NODE_PATH + '/assets/godIndex.html');
+        });
+    }
+
+    private loadCredentials(): any
+    {
+        const cert = fs.readFileSync(process.env.CERT_PATH + '/fullchain.pem')
+        const key = fs.readFileSync(process.env.CERT_PATH + '/privkey.pem')
+
+        return {
+            key: key,
+            cert: cert
+        };
     }
 }
 
