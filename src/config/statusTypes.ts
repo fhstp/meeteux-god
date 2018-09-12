8b5e19da9f5fe0643e72fbf73b019f718db24307
Niklas <niklas.thuer@gmail.com>
2018-09-12 14:10:14 +0200

    Changed ip adresses

--- src/config/statusTypes.ts
+++ src/config/statusTypes.ts
@@ -1,4 +1,4 @@
-export let ONLINE = 1
-export let OFFLINE = 2
-export let FREE = 3
-export let OCCUPIED = 4
+export let ONLINE = 1;
+export let OFFLINE = 2;
+export let FREE = 3;
+export let OCCUPIED = 4;
